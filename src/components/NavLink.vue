c3b049d2de5281b088398705cbcdc7e0d267fdd7
Rednata <maknatosh@gmail.com>
2024-09-18 11:29:20 +0700

    feat: create NavLink component for Head and Footer

---
+++ src/components/NavLink.vue
@@ -0,0 +1,34 @@
+<template>
+  <li :class="classItem" >
+    <router-link v-if="icon" :to="link">
+      <img :src="require(`@/assets/logo/${icon}`)" :alt="icon">
+    </router-link>
+    <router-link :to="link">
+      {{ text }}
+    </router-link>
+  </li>
+</template>
+
+<script>
+  export default {
+    props: {
+      classItem: String,
+      text: {
+        type: String,
+        required: true,
+        default: ''
+      },
+      link: {
+        type: String,
+        required: true,
+      },
+      icon: {
+        type: String,
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
