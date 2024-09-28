5a9dcad48ec40653d6f714d635220244a40a554a
Rednata <maknatosh@gmail.com>
2024-09-28 19:58:22 +0700

    add spinnerComponent

--- src/store/goodsItem.js
+++ src/store/goodsItem.js
@@ -1,5 +1,13 @@
 const goodsItem = {
   state: {
+    initGoodsItem: {
+      country: '',
+      description: '',
+      id: '',
+      image: '',
+      name: '',
+      price: ''
+    },
     goodsItem: {
       country: '',
       description: '',
@@ -7,21 +15,33 @@ const goodsItem = {
       image: '',
       name: '',
       price: ''
-    }
+    },
+    isLoading: false,
   },
   mutations: {
     setGoodsItemData(state, payload) {
       state.goodsItem = payload;
+    },
+    setIsLoading(state, payload) {
+      state.isLoading = payload;
     }
   },
   actions: {
-    setGoodsItemData({commit}, payload) {
-      commit('setGoodsItemData', payload)
+    setGoodsItemData({commit, state}, payload) {
+      if (payload === null) {
+        commit('setGoodsItemData', state.initGoodsItem);
+      } else commit('setGoodsItemData', payload)
+    },
+    setIsLoading({commit}, data) {
+      commit('setIsLoading', data)
     }
   },
   getters: {
     getGoodsItem(state) {
       return state.goodsItem;
+    },
+    isLoading(state) {
+      return state.isLoading;
     }
   }
 }
--- src/views/GoodsItemView.vue
+++ src/views/GoodsItemView.vue
@@ -7,11 +7,11 @@
             <nav-bar-component />
           </div>
         </div>
-        <h1 class="title-big" v-if="product">{{ product.name }}</h1>
+        <h1 class="title-big" v-if="!isLoading">{{ product.name }}</h1>
       </div>
     </div>
-
-    <section class="shop" v-if="product">
+    <spinner-component v-if="isLoading"/>
+    <section class="shop" v-else>
       <div class="container">
         <div class="row">
           <div class="col-lg-5 offset-1">
@@ -41,30 +41,38 @@
 
 <script>
 import NavBarComponent from '@/components/NavBarComponent.vue';
-
+import SpinnerComponent from '@/components/SpinnerComponent.vue';
   export default {
-  components: { NavBarComponent },
+  components: { NavBarComponent, SpinnerComponent },
   data() {
     return {
     }
   },
   mounted() {
+    this.$store.dispatch('setIsLoading', true)
     const pagePath = this.pageName === 'coffee' ? 'coffee' : 'goods'
     fetch(`http://localhost:3000/${pagePath}/${this.$route.params.id}`)
       .then(res => res.json())
-      .then(data => this.$store.dispatch('setGoodsItemData', data));
+      .then(data => {
+        setTimeout(() => {
+          this.$store.dispatch('setGoodsItemData', data)
+          this.$store.dispatch('setIsLoading', false)
+      }, 1000)
+      });
   },
   destroyed() {
-    this.product = null;
+    this.$store.dispatch('setGoodsItemData', null)
   },
   computed: {
     pageName() {
       return this.$route.name
     },
     product() {
-      const product = this.$store.getters['getGoodsItem'];
-      return product;
+      return this.$store.getters['getGoodsItem'];
     },
+    isLoading() {
+      return this.$store.getters['isLoading'];
+    }
   }
   }
 </script>
